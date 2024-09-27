3ec5a955bc41d87b692ecb36876cbab21da20841
spirit223 <2235569493@qq.com>
2024-09-27 22:27:31 +0800

    init

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
 # todo receive input, try to analysis language and translate
 # using http lib to send request and analysis response(json) and show result
+
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+
+class TranslateExtension(Extension):
+
+    def __init__(self):
+        super().__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        """
+        in this case `event` will be an instance of KeywordQueryEvent
+        on_event() may return an action (see Actions).
+        :param event: KeywordQueryEvent
+        :param extension:
+        :return: action
+        """
+        items = []
+        for i in range(5):
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                             name='Item %s' % i,
+                                             description='Item description %s' % i,
+                                             on_enter=HideWindowAction()))
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    TranslateExtension().run()
