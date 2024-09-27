4c6c7fe48f073c68ea35515b5dcb94e96600c062
spirit223 <2235569493@qq.com>
2024-09-27 23:18:37 +0800

    change example

--- translate/ExtensionKeywordListener.py
+++ translate/ExtensionKeywordListener.py
@@ -2,14 +2,13 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
-from .media import ICON_FILE
 
 
 class ExtensionKeywordListener(EventListener):
     def get_action_to_render(self, name, description, on_enter=None):
         item = ExtensionResultItem(name=name,
                                    description=description,
-                                   icon=ICON_FILE,
+                                   icon='images/icon.png',
                                    on_enter=on_enter or DoNothingAction())
 
         return RenderResultListAction([item])
@@ -17,4 +16,4 @@ class ExtensionKeywordListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_argument()
         return self.get_action_to_render(name="translate",
-                                         description="Example: yd apple %s " % query)
+                                         description="Example: yd apple %s " % query or '')
