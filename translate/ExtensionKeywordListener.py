223101fb278609bad1578ee576b3490926b4cb39
spirit223 <2235569493@qq.com>
2024-09-28 15:36:17 +0800

    add delay

--- translate/ExtensionKeywordListener.py
+++ translate/ExtensionKeywordListener.py
@@ -3,12 +3,20 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
 from translate.LanguageDiscriminator import ParseQueryError, LanguageDiscriminator
+from translate.PreferencesInfo import PreferencesInfo
 from translate.RequestBuilder import RequestBuilder
 import json
 import traceback
+import threading
 
 
+# todo: use timer judge whether user stop input
 class ExtensionKeywordListener(EventListener):
+    def __init__(self):
+        self.countdown_time = PreferencesInfo.delay
+        self.timer = None
+        self.is_running = False
+
     def get_action_to_render(self, name, description, on_enter=None):
         """
         generate result item
@@ -25,14 +33,21 @@ class ExtensionKeywordListener(EventListener):
         return RenderResultListAction([item])
 
     def on_event(self, event, extension):
+        if self.timer:
+            self.timer.cancel()
+        self.is_running = True
         query = event.get_argument()
-        if query is None:
+        self.timer = threading.Timer(self.countdown_time, self.callback(query))
+        self.timer.start()
+
+    def callback(self, text):
+        if text is None:
             return self.get_action_to_render(name="translate",
-                                         description="Example: yd apple")
+                                             description="Example: yd apple")
         else:
             try:
-                query = query.encode('utf-8').decode('utf-8')
-                res = RequestBuilder.build(query)
+                text = text.encode('utf-8').decode('utf-8')
+                res = RequestBuilder.build(text)
                 print(res.data)
                 # res.data.translation is str array contain translate result
                 translation_arr = json.loads(res.data)
@@ -41,13 +56,13 @@ class ExtensionKeywordListener(EventListener):
                     raise TranslateFailException("translate failed, non key 'translation'")
                 for item in translation_arr['translation']:
                     items.append(ExtensionResultItem(name=item,
-                                               description=item,
-                                               icon='images/icon.png',
-                                               on_enter=DoNothingAction()))
+                                                     description=item,
+                                                     icon='images/icon.png',
+                                                     on_enter=DoNothingAction()))
                 return RenderResultListAction(items)
             except ParseQueryError:
                 return self.get_action_to_render(name="Incorrect input",
-                                                 description="Example: yd apple %s" % query)
+                                                 description="Example: yd apple %s" % text)
             except TranslateFailException as e:
                 return self.get_action_to_render(name="translate failed",
                                                  description="reason: %s" % e)
@@ -55,6 +70,7 @@ class ExtensionKeywordListener(EventListener):
                 traceback.print_exc()
                 return self.get_action_to_render(name="extension error!",
                                                  description="%s" % e)
+
 
 class TranslateFailException(Exception):
     pass
--- translate/PreferencesInfo.py
+++ translate/PreferencesInfo.py
@@ -4,12 +4,14 @@ from ulauncher.api.client.EventListener import EventListener
 class PreferencesInfo:
     app_id = ''
     app_secrit = ''
+    delay = 2
 
 
 class PreferencesListener(EventListener):
     def on_event(self, event, extension):
         PreferencesInfo.app_id = event.preferences['appId']
         PreferencesInfo.app_secrit = event.preferences['appSecrit']
+        PreferencesInfo.app_secrit = event.preferences['delay']
 
 
 class PreferencesUpdateListener(EventListener):
@@ -18,3 +20,5 @@ class PreferencesUpdateListener(EventListener):
             PreferencesInfo.app_id = event.new_value
         elif event.id == 'appSecrit':
             PreferencesInfo.app_secrit = event.new_value
+        elif event.id == 'delay':
+            PreferencesInfo.delay = event.new_value
